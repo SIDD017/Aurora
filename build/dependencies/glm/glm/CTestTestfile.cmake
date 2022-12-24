e3ac15d706c0fa05372e5f4bae65a4fa18124710
SIDD017 <siddhartha.jejurkar5@gmail.com>
2022-12-24 23:39:46 +0530

    Fix: Cmake builds failing on windows platform
    - Add gitignore file
    - Remove build folder from remote origin
    - Rectify cmake config to use WIN32 when building on windows platform.# Please enter the commit message for your changes. Lines starting

--- build/dependencies/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/siddhartha/test_opengl/dependencies/glm/glm
-# Build directory: /home/siddhartha/test_opengl/build/dependencies/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- dependencies/stb_image/stb_image_impl.c
+++ dependencies/stb_image/stb_image_impl.c
@@ -1,5 +1,2 @@
-#ifndef STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
-
-#endif
