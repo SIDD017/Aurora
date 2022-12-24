148e66bb74403ef78119f4cf7c69e5616598cdc5
SIDD017 <f20180617@goa.bits-pilani.ac.in>
2022-12-24 16:35:26 +0530

    Cleanup: Remove build directory from remote

--- build/dependencies/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Aurora/Aurora/dependencies/glm/glm
-# Build directory: E:/Aurora/Aurora/build/dependencies/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
